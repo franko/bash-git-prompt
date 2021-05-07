2faecbf5c7e1e08f55efaaca3a88fc46b15a7872
Francesco <francesco.bbt@gmail.com>
2021-05-07 23:06:59 +0200

    msys2: add username instead of hostname

--- git-prompt-msys2.sh
+++ git-prompt-msys2.sh
@@ -30,12 +30,8 @@ git_branch_name () {
   fi
 }
 
-prompt_hostname () {
-  printf "\001\e[36m\002%s" "$HOSTNAME"
-}
-
 prompt_sys () {
   printf "\001\e[35m\002$MSYSTEM\001\e[0m\002 "
 }
 
-PS1="\$(prompt_hostname) \$(git_branch_name)\$(prompt_sys)\\[\e[33m\\]\w\\[\e[0m\\]$ "
+PS1="\\[\033[01;32m\\]\u\\[\033[00m\\] \$(git_branch_name)\$(prompt_sys)\\[\e[33m\\]\w\\[\e[0m\\]$ "
