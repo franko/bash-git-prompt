2ffc3758e1aa4a3347ee96f5b4fb312b5c998e6f
Francesco <francesco.bbt@gmail.com>
2021-05-07 10:17:25 +0200

    Create git-prompt-linux.sh

---
+++ git-prompt-linux.sh
@@ -0,0 +1,37 @@
+# See:
+#
+# https://unix.stackexchange.com/questions/28827/why-is-my-bash-prompt-getting-bugged-when-i-browse-the-history
+# https://superuser.com/questions/301353/escape-non-printing-characters-in-a-function-for-a-bash-prompt
+#
+# for explanation abour these crazy escape sequences. In short, to not break line editing you need to wrap
+# with '\[' '\]' zero-length part of the prompt. These are in turn equivalent to \001 and \002.
+
+git_branch_name () {
+  local xroot="$PWD"
+  while [ ! -e "${xroot}/.git" ]; do
+    local updir=${xroot%/*}
+    if [ "$updir" == "$xroot" ]; then
+      break
+    fi
+    xroot="$updir"
+  done
+  local xgit_dir="$xroot/.git"
+  local worktree_tag
+  local fline
+  if [ -f "$xgit_dir" ]; then
+    # if .git is a file it can be a worktree
+    read fline < .git
+    xgit_dir="${fline#gitdir: }"
+    worktree_tag=" (worktree)"
+  fi
+  if [ -f $xgit_dir/HEAD ]; then
+    read fline < $xgit_dir/HEAD
+    printf "[\001\e[34m\002${fline##*/}$worktree_tag\001\e[0m\002] "
+  fi
+}
+
+prompt_hostname () {
+  printf "\001\e[36m\002%s" "$HOSTNAME"
+}
+
+PS1="\u@\$(prompt_hostname) \$(git_branch_name)\\[\e[33m\\]\w\\[\e[0m\\] $ "
